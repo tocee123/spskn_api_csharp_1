87120b92b2378f8e6b271ad52b77e31d1c333840
adamvrg1 <vargaadam2007@gmail.com>
2023-03-02 20:05:31 +0100

    v2.1

--- Megoldasok/VargaAdam/divisors1/Program.cs
+++ Megoldasok/VargaAdam/divisors1/Program.cs
@@ -1,49 +1,110 @@
-﻿Console.WriteLine("This program will write out numbers between a closed interval that can be divided by the divisor.");
+﻿string infoOne = "The following numbers are in the (closed) interval you've given:";
 
-Console.WriteLine("What's the lower limit?");
-int lowerLimit = int.Parse(Console.ReadLine());
-Console.WriteLine("What's the upper limit?");
-int upperLimit = int.Parse(Console.ReadLine());
+
+Console.WriteLine("This program will write out numbers between a closed interval that can be divided by the divisor.");
+
+Console.WriteLine("What's the first limit?");
+int firstLimit = int.Parse(Console.ReadLine());
+Console.WriteLine("What's the second limit?");
+int secondLimit = int.Parse(Console.ReadLine());
 Console.WriteLine("Add a divisor number:");
 int divisorNumber = int.Parse(Console.ReadLine());
 
-Console.WriteLine("The following numbers are in the (closed) interval you've given:");
 
-for (int i = lowerLimit; i <= upperLimit; i++)
+if (firstLimit < secondLimit)
 {
-    Console.Write(i);
+    Console.WriteLine(infoOne);
 
-    if (i < upperLimit)
+    for (int i = firstLimit; i <= secondLimit; i++)
     {
-        Console.Write(", ");
-    }
+        Console.Write(i);
 
-    else if (i == upperLimit)
-    {
-        Console.Write(".");   
+        if (i < secondLimit)
+        {
+            Console.Write(", ");
+        }
+
+        else if (i == secondLimit)
+        {
+            Console.Write(".");
+        }
     }
+}
+
+else if (firstLimit > secondLimit)
+{
+    Console.WriteLine(infoOne);
+
+    for (int k = secondLimit; k <= firstLimit; k++)
+    {
+        Console.Write(k);
+
+        if (k < firstLimit)
+        {
+            Console.Write(", ");
+        }
+
+        else if (k == firstLimit)
+        {
+            Console.Write(".");
+        }
+    }
+}
+
+else if (firstLimit == secondLimit)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("The two borders cannot be equal to each other.");
+    Console.ResetColor();
+    Console.ReadLine();
+    Environment.Exit(0);
 }
 
 Console.WriteLine();
 Console.WriteLine();
-Console.WriteLine($"Numbers between {lowerLimit} and {upperLimit} that can be divided by {divisorNumber}:");
+Console.WriteLine($"Numbers between {firstLimit} and {secondLimit} that can be divided by {divisorNumber}:");
 
-for (int i = lowerLimit; i <= upperLimit; i++)
+if (firstLimit < secondLimit)
 {
-    if (i == 0)
+    for (int i = firstLimit; i <= secondLimit; i++)
     {
-        continue;
-    }
+        if (i == 0)
+        {
+            continue;
+        }
 
-    
-    if (i % divisorNumber == 0)
-    {
-        Console.Write(i + ", ");
-    }
 
-    if (i == upperLimit)
+        if (i % divisorNumber == 0)
+        {
+            Console.Write(i + ", ");
+        }
+
+        if (i == secondLimit)
+        {
+            Console.Write(".");
+        }
+    }
+}
+
+else if (firstLimit > secondLimit)
+{
+    for (int j = secondLimit; j <= firstLimit; j++)
     {
-        Console.Write(".");
+        if (j == 0)
+        {
+            continue;
+        }
+
+
+        if (j % divisorNumber == 0)
+        {
+            Console.Write(j + ", ");
+        }
+
+        if (j == firstLimit)
+        {
+            Console.Write(".");
+        }
     }
 }
 
