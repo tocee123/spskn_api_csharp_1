e17a96cf2c1c5ed53450340e79ec9e94eecde9e2
ttoroknd <tamas.torok@newday.com>
2023-03-06 22:51:23 +0000

    New folders

---
+++ Megoldasok/create.bat
@@ -0,0 +1,42 @@
+SET NewFolder=01_ConsoleApplication
+
+cd c:\Users\S01764\Documents\GitHub\spskn_api_csharp_1\Megoldasok\BogiPatrik\
+mkdir %NewFolder%
+cd %NewFolder%
+dotnet new sln
+dotnet new console --output ConsoleApplication
+dotnet sln add ConsoleApplication
+cd c:\Users\S01764\Documents\GitHub\spskn_api_csharp_1\Megoldasok\DomonkosBalint\
+mkdir %NewFolder%
+cd %NewFolder%
+dotnet new sln
+dotnet new console --output ConsoleApplication
+dotnet sln add ConsoleApplication
+cd c:\Users\S01764\Documents\GitHub\spskn_api_csharp_1\Megoldasok\MolnarSamuel\
+mkdir %NewFolder%
+cd %NewFolder%
+dotnet new sln
+dotnet new console --output ConsoleApplication
+dotnet sln add ConsoleApplication
+cd c:\Users\S01764\Documents\GitHub\spskn_api_csharp_1\Megoldasok\SzeghyTamas\
+mkdir %NewFolder%
+cd %NewFolder%
+dotnet new sln
+dotnet new console --output ConsoleApplication
+dotnet sln add ConsoleApplication
+cd c:\Users\S01764\Documents\GitHub\spskn_api_csharp_1\Megoldasok\SzlanicskaMarian\
+mkdir %NewFolder%
+cd %NewFolder%
+dotnet new sln
+dotnet new console --output ConsoleApplication
+dotnet sln add ConsoleApplication
+cd c:\Users\S01764\Documents\GitHub\spskn_api_csharp_1\Megoldasok\TakacsDominik\
+mkdir %NewFolder%
+cd %NewFolder%
+dotnet new sln
+dotnet new console --output ConsoleApplication
+dotnet sln add ConsoleApplication
+cd c:\Users\S01764\Documents\GitHub\spskn_api_csharp_1\Megoldasok\TamasMiklos\
+dotnet new sln
+dotnet new console --output ConsoleApplication
+dotnet sln add ConsoleApplication
