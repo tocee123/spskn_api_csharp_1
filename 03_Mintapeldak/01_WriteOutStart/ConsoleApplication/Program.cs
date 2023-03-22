a63f4899fcc21b14a9b5ee811df9281749e44036
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-03-22 22:42:38 +0000

    New files (#10)
    Co-authored-by: ttoroknd <tamas.torok@newday.com>

---
+++ 03_Mintapeldak/01_WriteOutStart/ConsoleApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberOfSpaces = 0;
+
+do
+{
+    Console.Write("Adjal meg egy egesz szamot 0 es 20 kozott:");
+    if (int.TryParse(Console.ReadLine(), out numberOfSpaces) && 0 < numberOfSpaces && numberOfSpaces < 20)
+    {
+        break;
+    }
+} while (true);
+
+Console.WriteLine($"{new string(' ', numberOfSpaces)}START");
+
--- 03_Mintapeldak/readme.md
+++ 03_Mintapeldak/readme.md
@@ -1,1 +1,6 @@
 # Mintapeldak kovetkeznek
+
+| Konyvtar | Leiras|
+--|--
+|  `01_WriteOutStart`| Felhasznalotol beker 20nal nem nagyobb szamot, es annyi szokozt tesz a **START** szo ele
+
