4ec10f10e848e3876d1bd90cc965286769de67f0
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-06-04 12:54:57 +0200

    New attempt

--- Megoldasok/Torok/32_Fibonacci/ConsoleApplication/FibonacciCalculator.cs
+++ Megoldasok/Torok/32_Fibonacci/ConsoleApplication/FibonacciCalculator.cs
@@ -2,8 +2,19 @@
 
 public class FibonacciCalculator
 {
-    public static object CalculateNthElement(int input)
+    public static int CalculateNthElement(int input)
     {
-        throw new NotImplementedException();
+        if (input == 0) return 0;
+        if (input == 1) return 1;
+        int previous = 1;
+        int result = 1;
+
+        for (int i = 2; i<input;i++)
+        {
+            previous = result;
+            result += previous;            
+        }
+
+        return result;
     }
 }
