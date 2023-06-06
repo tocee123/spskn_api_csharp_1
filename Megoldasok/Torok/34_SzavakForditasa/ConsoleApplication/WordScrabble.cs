d6315fe8b1fa9bd26b61bbf26b1a01b9a5165eb0
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-06-06 21:35:13 +0200

    Fixing tests

--- Megoldasok/Torok/34_SzavakForditasa/ConsoleApplication/WordScrabble.cs
+++ Megoldasok/Torok/34_SzavakForditasa/ConsoleApplication/WordScrabble.cs
@@ -3,8 +3,9 @@
 public class WordScrabble
 {
     public static string ReverseOnlyLetters(string sentence)
-    => sentence.Split(" ")
-            .Select(s => s.ToCharArray().Reverse().Select(c => c.ToString()).JoinArray("")).JoinArray();
+    => sentence?.Split(" ")
+            .Select(s => s.ToCharArray().Reverse().Select(c => c.ToString()).JoinArray("")).JoinArray()
+        ?? sentence;
 
     public static object ReverseOnlyWords(string input)
     {
