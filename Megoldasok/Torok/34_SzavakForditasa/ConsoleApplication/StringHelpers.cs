1541f2e4fc2b0b75e86192d9b7b8b04a8ab8b404
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-06-06 21:54:33 +0200

    CallSteps

--- Megoldasok/Torok/34_SzavakForditasa/ConsoleApplication/StringHelpers.cs
+++ Megoldasok/Torok/34_SzavakForditasa/ConsoleApplication/StringHelpers.cs
@@ -4,4 +4,7 @@ public static class StringHelpers
 {
     public static string JoinArray(this IEnumerable<string> strings, string separator = " ")
         => string.Join(separator, strings);
+
+    public static IEnumerable<string> Callmethod(this IEnumerable<string> strings, Func<IEnumerable<string>, IEnumerable<string>> func)
+        => func(strings);
 }
--- Megoldasok/Torok/34_SzavakForditasa/ConsoleApplication/WordScrabble.cs
+++ Megoldasok/Torok/34_SzavakForditasa/ConsoleApplication/WordScrabble.cs
@@ -3,18 +3,28 @@
 public class WordScrabble
 {
     public static string ReverseOnlyLetters(string sentence)
-    => sentence?.Split()
-            .Select(ReverseLetters).JoinArray()
+    => sentence?.Split().Select(ReverseLetters).JoinArray()
         ?? sentence;
 
-    public static object ReverseOnlyWords(string sentence)
+    public static string ReverseOnlyWords(string sentence)
     => sentence?.Split().Reverse().JoinArray()
         ?? sentence;
 
-    public static object ReverseWordsAndLetters(string sentence)
-    => sentence?.Split()
-            .Select(ReverseLetters).Reverse().JoinArray()
+    public static string ReverseWordsAndLetters(string sentence)
+    => sentence?.Split().Select(ReverseLetters).Reverse().JoinArray()
         ?? sentence;
+
+    private static string CallSteps(string sentence, Func<string, string> reverseLetters, Func<IEnumerable<string>, IEnumerable<string>> reverseWords)
+    {
+        if (sentence == null)
+        {
+            return sentence;
+        }
+        return sentence.Split()
+            .Select(reverseLetters)
+            .Callmethod(reverseWords)
+            .JoinArray();
+    }
 
     private static string ReverseLetters(string s)
     => s.ToCharArray().Reverse().Select(c => c.ToString()).JoinArray("");
