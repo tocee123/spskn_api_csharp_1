a8dc26f090728598ff45ce757449097f9eff8b23
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-06-06 21:59:21 +0200

    updating logic

--- Megoldasok/Torok/34_SzavakForditasa/ConsoleApplication/WordScrabble.cs
+++ Megoldasok/Torok/34_SzavakForditasa/ConsoleApplication/WordScrabble.cs
@@ -3,16 +3,13 @@
 public class WordScrabble
 {
     public static string ReverseOnlyLetters(string sentence)
-    => sentence?.Split().Select(ReverseLetters).JoinArray()
-        ?? sentence;
+    => CallSteps(sentence, ReverseLetters, s => s);
 
     public static string ReverseOnlyWords(string sentence)
-    => sentence?.Split().Reverse().JoinArray()
-        ?? sentence;
+    => CallSteps(sentence, s => s, s => s.Reverse());
 
     public static string ReverseWordsAndLetters(string sentence)
-    => sentence?.Split().Select(ReverseLetters).Reverse().JoinArray()
-        ?? sentence;
+    => CallSteps(sentence, ReverseLetters, s => s.Reverse());
 
     private static string CallSteps(string sentence, Func<string, string> reverseLetters, Func<IEnumerable<string>, IEnumerable<string>> reverseWords)
     {
