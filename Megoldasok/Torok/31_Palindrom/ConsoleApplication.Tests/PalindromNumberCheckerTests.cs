be57e88a957082d014a25df0cf07d4953118bd18
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-06-08 11:47:19 +0200

    PalindromNumberChecker added

---
+++ Megoldasok/Torok/31_Palindrom/ConsoleApplication.Tests/PalindromNumberCheckerTests.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApplication.Tests;
+
+public class PalindromNumberCheckerTests
+{
+    [Theory]
+    [InlineData(101, true)]
+    [InlineData(454, true)]
+    [InlineData(111, true)]
+    [InlineData(987, false)]
+    public void IsPalindrom_WhenInputIsGiven_ReturnsExpected(int input, bool expected)
+    {
+        PalindromNumberChecker.IsPalindrom(input).Should().Be(expected);
+    }
+}
---
+++ Megoldasok/Torok/31_Palindrom/ConsoleApplication/PalindromNumberChecker.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApplication;
+
+public class PalindromNumberChecker
+{
+    public static bool IsPalindrom(int number)
+    {
+        var exponentSearch = number;
+        var exponent = 0;
+        while (exponentSearch > 0)
+        {
+            exponent++;
+            exponentSearch /= PowerOf10(exponent);
+        }
+        for (int i = 0; i < exponent; i++)
+        {
+            var leftNumber = number / PowerOf10(exponent - i);
+            var rightNumber = number % PowerOf10(i);
+            if (leftNumber != rightNumber)
+            {
+                return false;
+            }
+            number /= PowerOf10(exponent - i);
+        }
+
+        return true;
+    }
+
+    private static int PowerOf10(int exponent)
+        => (int)Math.Pow(10, exponent);
+}
