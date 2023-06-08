35dfa4aaa3035ee4436208151b9cf8803e0a8ff7
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-06-08 12:16:30 +0200

    Simplifying method

--- Megoldasok/Torok/31_Palindrom/ConsoleApplication/PalindromNumberChecker.cs
+++ Megoldasok/Torok/31_Palindrom/ConsoleApplication/PalindromNumberChecker.cs
@@ -7,15 +7,21 @@ public class PalindromNumberChecker
         var exponent = FindExponent(number);
         for (int i = 0; i < exponent; i++)
         {
-            var leftNumber = (number / PowerOf10(exponent - i)) % 10;
-            var rightNumber = (number / PowerOf10(i)) % 10;
-            if (leftNumber != rightNumber)
+
+            if (!AreTwoSideNumbersEqual(number, exponent, i))
             {
                 return false;
             }
         }
 
         return true;
+    }
+
+    private static bool AreTwoSideNumbersEqual(int number, int maxExponent, int currentExponent)
+    {
+        var leftNumber = (number / PowerOf10(maxExponent - currentExponent)) % 10;
+        var rightNumber = (number / PowerOf10(currentExponent)) % 10;
+        return leftNumber == rightNumber;
     }
 
     private static int FindExponent(int number)
