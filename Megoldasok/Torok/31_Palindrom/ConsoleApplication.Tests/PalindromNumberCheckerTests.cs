d8b51f3e314396750437ffa146469f30803a676b
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-06-08 12:09:33 +0200

    Found logic

--- Megoldasok/Torok/31_Palindrom/ConsoleApplication.Tests/PalindromNumberCheckerTests.cs
+++ Megoldasok/Torok/31_Palindrom/ConsoleApplication.Tests/PalindromNumberCheckerTests.cs
@@ -6,7 +6,10 @@ public class PalindromNumberCheckerTests
     [InlineData(101, true)]
     [InlineData(454, true)]
     [InlineData(111, true)]
+    [InlineData(44, true)]
     [InlineData(987, false)]
+    [InlineData(1010, false)]
+    [InlineData(1001, true)]
     public void IsPalindrom_WhenInputIsGiven_ReturnsExpected(int input, bool expected)
     {
         PalindromNumberChecker.IsPalindrom(input).Should().Be(expected);
--- Megoldasok/Torok/31_Palindrom/ConsoleApplication/PalindromNumberChecker.cs
+++ Megoldasok/Torok/31_Palindrom/ConsoleApplication/PalindromNumberChecker.cs
@@ -4,25 +4,30 @@ public class PalindromNumberChecker
 {
     public static bool IsPalindrom(int number)
     {
-        var exponentSearch = number;
-        var exponent = 0;
-        while (exponentSearch > 0)
-        {
-            exponent++;
-            exponentSearch /= PowerOf10(exponent);
-        }
+        var exponent = FindExponent(number);
         for (int i = 0; i < exponent; i++)
         {
-            var leftNumber = number / PowerOf10(exponent - i);
-            var rightNumber = number % PowerOf10(i);
+            var leftNumber = (number / PowerOf10(exponent - i)) % 10;
+            var rightNumber = (number / PowerOf10(i)) % 10;
             if (leftNumber != rightNumber)
             {
                 return false;
             }
-            number /= PowerOf10(exponent - i);
         }
 
         return true;
+    }
+
+    private static int FindExponent(int number)
+    {
+        var exponentSearch = number;
+        var exponent = -1;
+        while (exponentSearch > 0)
+        {
+            exponent++;
+            exponentSearch /= 10;
+        }
+        return exponent;
     }
 
     private static int PowerOf10(int exponent)
