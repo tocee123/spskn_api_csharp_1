fabd334c8687215e9cb151c0eaa5e3a84ee0985f
Kitsune713 <123865543+Kitsune713@users.noreply.github.com>
2023-06-29 20:00:14 +0200

    Edited Fibonacci Program.cs
    Nem engedte feltölteni a .cs fájlt

--- Megoldasok/MolnarSamuel/32_Fibonacci/ConsoleApplication/Program.cs
+++ Megoldasok/MolnarSamuel/32_Fibonacci/ConsoleApplication/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+static void Fibonacci(int length)
+{
+    int a = 0;
+    int b = 1;
+    int outputNum = 0;
+    Console.WriteLine("Do you want to see all the numbers or only the last one?\n0 -> all\n1 -> only one");
+    string choice = Console.ReadLine();
+    Console.ResetColor();
+    if (choice == "0")
+    {
+        Console.Write($"{a}, {b}");
+        for (int i = 1; i < length; i++)
+        {
+            outputNum = a + b;
+            Console.Write($", {outputNum}");
+            a = b;
+            b = outputNum;
+        }
+    }
+    else
+    {
+        for (int i = 1; i < length; i++)
+        {
+            outputNum = a + b;
+            
+            a = b;
+            b = outputNum;
+        }
+        Console.WriteLine(outputNum);
+    }
+}
+
+Console.WriteLine("In the Fibonacci series which positioned number do you want to see?");
+Console.ForegroundColor = ConsoleColor.Cyan;
+int length = int.Parse(Console.ReadLine());
+Fibonacci(length);
