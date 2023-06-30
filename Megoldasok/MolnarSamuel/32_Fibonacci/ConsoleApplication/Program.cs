99984d83c0241b772a7fa132b9f3d9ebd94be3a9
Kitsune713 <123865543+Kitsune713@users.noreply.github.com>
2023-06-30 18:33:13 +0200

    Program.cs updated

--- Megoldasok/MolnarSamuel/32_Fibonacci/ConsoleApplication/Program.cs
+++ Megoldasok/MolnarSamuel/32_Fibonacci/ConsoleApplication/Program.cs
@@ -1,36 +1,47 @@
-static void Fibonacci(int length)
+static void CalculateFibonacci(int length)
 {
+    var FibonacciNumbers = new int[length];
     int a = 0;
     int b = 1;
     int outputNum = 0;
+
+    FibonacciNumbers[0] = 0;
+    FibonacciNumbers[1] = 1;
+    
+    for (int i = 1; i < length; i++)
+    {
+        outputNum = a + b;
+        FibonacciNumbers[i] = outputNum;
+        a = b;
+        b = outputNum;
+    }
+    DisplayFibonacci(length, FibonacciNumbers);
+}
+
+static void DisplayFibonacci(int length, int[] FibonacciNumbers) 
+{
     Console.WriteLine("Do you want to see all the numbers or only the last one?\n0 -> all\n1 -> only one");
     string choice = Console.ReadLine();
     Console.ResetColor();
-    if (choice == "0")
+    Console.ForegroundColor = ConsoleColor.Green;
+    if (choice == "0") 
     {
-        Console.Write($"{a}, {b}");
-        for (int i = 1; i < length; i++)
+        Console.Write("0, 1");
+        for (int i = 1; i < FibonacciNumbers.Length; i++)
         {
-            outputNum = a + b;
-            Console.Write($", {outputNum}");
-            a = b;
-            b = outputNum;
+            Console.Write($", {FibonacciNumbers[i]}");
         }
     }
-    else
+    
+    else 
     {
-        for (int i = 1; i < length; i++)
-        {
-            outputNum = a + b;
-            
-            a = b;
-            b = outputNum;
-        }
-        Console.WriteLine(outputNum);
+        Console.WriteLine(FibonacciNumbers[length - 1]);
     }
+    Console.ResetColor();
 }
 
 Console.WriteLine("In the Fibonacci series which positioned number do you want to see?");
 Console.ForegroundColor = ConsoleColor.Cyan;
 int length = int.Parse(Console.ReadLine());
-Fibonacci(length);
+
+CalculateFibonacci(length);
